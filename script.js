ef0ea993721866a2432a463192340aba54b151eb
Pablo-Richardson <pabloorichardson@gmail.com>
2025-06-10 08:45:29 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleDarkMode() {
+    const body = document.body;
+    const darkModeButton = document.getElementById('darkModeButton');
+
+    // Toggle the "dark-mode" class on the body
+    body.classList.toggle('dark-mode');
+
+    // Change the button text
+    if (body.classList.contains('dark-mode')) {
+        darkModeButton.textContent = 'Light Mode';
+    } else {
+        darkModeButton.textContent = 'Dark Mode';
+    }
+}
+
+let clickCount = 1;
+function changeMessage()
+{
+    messages = 
+    [
+        "Test",
+        "Change"
+    ]
+    document.getElementById('button_change_text').textContent = messages[clickCount]
+    clickCount ++;
+    if(clickCount === messages.length)
+    {
+        clickCount = 0;
+    }
+}
